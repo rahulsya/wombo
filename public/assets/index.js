a4c2805d4e0fe70fd0a44cdea1140411a1f2c3f0
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-03-16 23:16:47 +0700

    add new assets image type png and svg

--- public/assets/index.js
+++ public/assets/index.js
@@ -2,6 +2,16 @@ export * from "./icons";
 import ShapeSectionTwo from "./ShapeSectionTwo.svg";
 
 import ShapeSection3 from "./ShapeSection3.svg";
+import ShapeSectionFour from "./ShapeSectionFour.svg";
 import ImgSectionTwo from "./SectionTwo.png";
+import ImageSectionFour from "./ImageSectionFour.png";
+import ImageSectionFive from "./ImageSectionFive.png";
 
-export { ShapeSectionTwo, ImgSectionTwo, ShapeSection3 };
+export {
+  ShapeSectionTwo,
+  ImgSectionTwo,
+  ShapeSection3,
+  ImageSectionFour,
+  ShapeSectionFour,
+  ImageSectionFive,
+};
