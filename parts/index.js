b0fcf120c2255b19efbbeed53814a48d5991eefb
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-03-16 23:24:52 +0700

    export component section 1 & 2

--- parts/index.js
+++ parts/index.js
@@ -1,4 +1,6 @@
 import Hero from "./Hero";
 import SectionOne from "./Section1";
 import SectionTwo from "./Section2";
-export { Hero, SectionOne, SectionTwo };
+import SectionThree from "./Section3";
+import SectionFour from "./Section4";
+export { Hero, SectionOne, SectionTwo, SectionThree, SectionFour };
