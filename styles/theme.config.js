9e9750d1d9a799310c9a87d0b4cdd51a751cae06
rahulsya <48597821+rahulsya@users.noreply.github.com>
2022-03-16 23:15:22 +0700

    add font size and font weight

--- styles/theme.config.js
+++ styles/theme.config.js
@@ -1,17 +1,36 @@
 import { createGlobalStyle } from "styled-components";
 
 export const theme = {
+  // color
   primary: "#3B82F6",
   dark: "#193766",
   muted: "#697B98",
   primaryLight: "#C6DBFF",
   mutedLight: "#FFFFFF",
 
+  // screen size
   screen: {
     huge: "1440px",
     large: "1170px",
     medium: "768px",
     small: "450px",
+  },
+
+  // font size
+  fontSize: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    base: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+  },
+
+  // font weight
+  fontWeight: {
+    normal: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
   },
 };
 
